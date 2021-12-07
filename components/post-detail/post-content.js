db68bbafbc9c6b77d913417e3e4b4e30748c6fde
Prajwal Adhikari <prajwalad101@gmail.com>
2021-12-07 13:35:17 +0545

    add helper functions to extract data from markdown

--- components/post-detail/post-content.js
+++ components/post-detail/post-content.js
@@ -1,12 +1,15 @@
+import ReactMarkdown from 'react-markdown';
+
 import PostHeader from './post-header';
 import dummyData from '../../utils/dummy-data';
 
 function PostContent() {
-  const imagePath = `/images/posts/${dummyData[0].slug}/${dummyData[0].image}`;
+  const dummyPost = dummyData[0];
+  const imagePath = `/images/posts/${dummyPost.slug}/${dummyPost.image}`;
   return (
     <article>
-      <PostHeader title={dummyData[0].title} image={imagePath} />
-      CONTENT
+      <PostHeader title={dummyPost.title} image={imagePath} />
+      <ReactMarkdown>{dummyPost.content}</ReactMarkdown>
     </article>
   );
 }
---
+++ posts/the-future-of-react-state-management.md
@@ -0,0 +1,10 @@
+---
+title: 'The Future of React State Management'
+date: '2022-10-16'
+image: 'the-future-of-react-state-management.png'
+excerpt: 'Learn about the history of state management in React and what the preferred solutions are today'
+---
+
+#This is a title
+
+This is a regular text with a [link] (https://google.com)
